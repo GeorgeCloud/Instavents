7a06e55248306cb21a266257d53ba47050424560
GeorgeCloud <net.george.sky@gmail.com>
2022-03-15 16:26:22 -0700

    refactor: signup route

--- api/auth/routes.py
+++ api/auth/routes.py
@@ -7,25 +7,25 @@ import bcrypt
 auth = Blueprint("auth", __name__)
 
 @auth.route('/signup', methods=['POST']) # Sign up Page
-def signup(): 
+def signup():
     name = request.json['name']
     phone_number = request.json['phone_number']
-    password = request.json['password']
-    # Added str
-    hashpass = bcrypt.hashpw(password.decode('utf-8'), bcrypt.gensalt())
-    # print(hashpass)
-    find_phone_number = users.find({'phone_number': phone_number})
+    user_exists = users.find_one({'phone_number': phone_number})
 
-    for data in find_phone_number:
-        if data:
-            return({'Error' : 'The phone number already exists!'})
-    # Added this
+    if user_exists:
+        return({'Error' : 'The phone number already exists!'})
+
+    password = request.json['password']
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
     if len(phone_number) == 12: # +1 123-456-7890
-        user_id = users.insert_one({'_id': uuid.uuid4().hex, 'name': name, 'phone_number': phone_number, 'password': hashpass }).inserted_id
-        # user_id = users.insert_one({'_id': uuid.uuid4().hex, 'name': name, 'phone_number': phone_number, 'password': password }).inserted_id
-        # session['phone_number'] = phone_number
-        look_user = users.find_one({'_id' : user_id})
-        return jsonify(look_user)
+        user_id = users.insert_one({
+            '_id': uuid.uuid4().hex, 'name': name, 'phone_number': phone_number,
+            'password': hashed_password }
+        ).inserted_id
+
+        new_user = users.find_one({'_id': user_id})
+        return jsonify([{'name' : new_user['name'], 'phone_number': new_user['phone_number']}])
     else:
         return({'Error' : 'Type the correct number'})
 
@@ -33,7 +33,7 @@ def signup():
 def login():
     return 'Login Page'
 
-@auth.route('/logout', methods=['GET'])  # Change to post after 
+@auth.route('/logout', methods=['GET'])  # Change to post after
 def logout():
     return redirect(url_for('auth.login'))
 
@@ -43,5 +43,3 @@ def get_users():
     for user in users.find():
         output.append({'name' : user['name'], 'phone_number': user['phone_number'], 'password': user['password']})
     return jsonify({'result' : output})
-
-
