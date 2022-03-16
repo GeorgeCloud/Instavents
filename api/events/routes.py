acf944230f6c542c4c580386fee035f6af33fb11
Ahyeonn <ahyeon971030@gmail.com>
2022-03-16 15:29:42 -0700

    fix: events routes

--- api/events/routes.py
+++ api/events/routes.py
@@ -11,6 +11,21 @@ def index():
 
     return jsonify({'meetings result' : output}), 200
 
+@event.route('/create', methods=['POST'])
+def create_event():
+    owner_id = request.json['owner_id'] or None
+    name = request.json['name']
+    event_name = request.json['event_name']
+    recipients = request.json['recipients']
+    date = request.json['date']
+    time = request.json['time']
+
+    event_id = events.insert_one({'_id': uuid.uuid4().hex, owner_id: None, 'name': name, 'event_name': event_name, 'date': date, 'recipients': recipients, 'time': time}).inserted_id
+
+    new_event = events.find_one({'_id' : event_id})
+    return jsonify(new_event), 200
+
+#If signed in
 @event.route('/user/<user_id>', methods=['GET'])
 def get_events_by_user(user_id):
     output = [e for e in events.find({'owner_id': user_id})]
@@ -27,16 +42,3 @@ def show_event(event_id):
 
     return jsonify({'event result' : 'not found'}), 404
 
-@event.route('/new', methods=['POST'])
-def new_event():
-    owner_id = request.json['owner_id'] or None
-    name = request.json['name']
-    event_name = request.json['event_name']
-    recipients = request.json['recipients']
-    date = request.json['date']
-    time = request.json['time']
-
-    event_id = events.insert_one({'_id': uuid.uuid4().hex, owner_id: None, 'name': name, 'event_name': event_name, 'date': date, 'recipients': recipients, 'time': time}).inserted_id
-
-    new_event = events.find_one({'_id' : event_id})
-    return jsonify(new_event), 200
