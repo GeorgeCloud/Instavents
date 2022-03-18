1ee0ef8dd9ca265fd498a05187734a5e4f0caf25
GeorgeCloud <net.george.sky@gmail.com>
2022-03-18 14:55:15 -0700

    feat: send text messages to invited guests(twilio)

--- api/twilio_api.py
+++ api/twilio_api.py
@@ -14,27 +14,27 @@ twilio_phone_number = os.getenv('TWILIO_PHONE_NUMBER')
 
 client = Client(account_sid, auth_token)
 
-def create_rsvp(recipients, event_id):
+def create_rsvp(recipient, event_id):
     # TODO: Validate unqiue entry: phone_number & event_id
-    for recipient, phone_number in recipients.items():
-        if validate_number(phone_number):
-            new_rsvp = {
-                '_id': uuid.uuid4().hex,
-                'event_id': event_id,
-                'recipient_name': recipient,
-                'recipient_phone_number': phone_number,
-                'status': None,
-                'created_on': datetime.now()
-            }
+    phone_number = recipient['number']
+    if validate_number(phone_number):
+        new_rsvp = {
+            '_id': uuid.uuid4().hex,
+            'event_id': event_id,
+            'recipient_name': recipient['name'],
+            'recipient_phone_number': phone_number,
+            'status': None,
+            'created_on': datetime.now()
+        }
 
-            rsvp_id = rsvps.insert_one(new_rsvp).inserted_id
-            events.update_one(
-               { '_id': event_id },
-               { '$addToSet': { 'rsvps': rsvp_id } }
-            )
+        rsvp_id = rsvps.insert_one(new_rsvp).inserted_id
+        events.update_one(
+           { '_id': event_id },
+           { '$addToSet': { 'rsvps': rsvp_id } }
+        )
 
-            client.messages.create(
-                to=f'{phone_number}',
-                from_=f'{twilio_phone_number}',
-                body=f'https://instavents.herokuapp.com/rsvp/{rsvp_id}'
-            )
+        client.messages.create(
+            to=f'{phone_number}',
+            from_=f'{twilio_phone_number}',
+            body=f'https://instavents.herokuapp.com/rsvp/{rsvp_id}'
+        )
