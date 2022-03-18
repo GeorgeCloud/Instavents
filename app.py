9eb47708809689ed4d153c696eb2a95d51bed853
GeorgeCloud <net.george.sky@gmail.com>
2022-03-18 15:34:01 -0700

    create key

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ from api.auth.routes import auth
 from api.users.routes import user
 from api.events.routes import event
 from web.routes import main
+import os
 
 app = Flask(__name__, template_folder="web/templates")
-app.secret_key = 'georgeandahyeon777'
+app.secret_key = os.getenv('SECRET_KEY')
 
 app.register_blueprint(auth, url_prefix='/')
 app.register_blueprint(user, url_prefix='/users')
